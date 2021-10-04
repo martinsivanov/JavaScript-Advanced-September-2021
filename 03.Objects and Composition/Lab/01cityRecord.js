deb5b54df24539cdf26901b4306b883ad7ca9590
martinsivanov <82410157+martinsivanov@users.noreply.github.com>
2021-10-04 12:10:54 +0300

    Adding solutions from lab.

---
+++ 03.Objects and Composition/Lab/01cityRecord.js
@@ -0,0 +1,17 @@
+function createCity(name,population,treasury) {
+    let city = {
+        name,
+        population,
+        treasury
+    };
+    return city;
+}
+
+console.log(createCity('Tortuga',
+7000,
+15000
+));
+console.log(createCity('Santo Domingo',
+12000,
+23500
+));
